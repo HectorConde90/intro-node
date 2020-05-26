aa3b7d43aad4351bed549748fd1aef1f020dfa4c
hectorconde90 <hecopa50@gmail.com>
2020-05-26 12:03:29 +0200

    Tests terminados

--- index.js
+++ index.js
@@ -2,20 +2,69 @@ export default class SortedList {
  
     constructor() {
         this.items = [];
-        this.length = this.items.length;
+        this.length = 0;
         }
       
     add(item) {
         this.items.push(item);
+        this.items.sort(function (a, b) {
+            return a - b;
+        });
+         this.length = this.items.length;
+        
         }
       
-        get(pos) {}
+    get(pos) {
+        if (pos > this.items.length) {
+            throw new Error('OutOfBounds');
+        } else {
+            return this.items[pos];
+        }
+        }
       
-        max() {}
+    max() {
+        if (this.items.length === 0) {
+            throw new Error('EmptySortedList');
+        } else {
+            return this.items[this.items.length - 1]
+        }
+
+            
+        }
       
-        min() {}
-      
-        sum() {}
-      
-        avg() {}
+    min() {
+        if (this.items.length === 0) {
+            throw new Error('EmptySortedList');
+        } else {
+            return this.items[0];
+        }
+
     }
+      
+    sum() {
+        let pos = 0;
+        if (this.items.length === 0) {
+            return 0;
+        } else {
+            this.items.forEach(element => {
+                return pos += element;
+            })
+            return pos;
+        }
+    }
+      
+    avg() { 
+        let pos = 0;
+        if (this.items.length === 0) {
+            throw new Error('EmptySortedList');
+        } else {
+            this.items.forEach(element => {
+                return pos += element;
+            })
+            return pos / this.items.length;
+        }
+    }
+    
+    
+    }
+
